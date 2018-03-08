c06a6417ca342f1a14da8f1e87768cf5dbb74f27
Gustavo Lobato <gustavo@gpsolucoes.com>
2018-03-07 23:05:33 -0300

    Versão testavel

--- app/src/main/java/com/example/gustavolobato/imc/MainActivity.java
+++ app/src/main/java/com/example/gustavolobato/imc/MainActivity.java
@@ -11,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void btnCalcularOnClick(View v){
 
+        TextView lblResultado = (TextView)findViewById(R.id.lblResultado);
+        EditText txtPeso = (EditText) findViewById(R.id.txtPeso);
+        EditText txtAltura = (EditText) findViewById(R.id.txtAltura);
+
+        int peso = Integer.parseInt(txtPeso.getText().toString());
+        float altura = Float.parseFloat(txtAltura.getText().toString());
+
+        float resultado = peso / (altura * altura);
+        if(resultado < 19){
+            //abaixo
+            lblResultado.setText("Abaixo do peso!");
+        }
+        else if(resultado > 32){
+            //obeso
+            lblResultado.setText("Acima do peso!");
+        }
+        else{
+            //ok
+            lblResultado.setText("Peso ok!");
+        }
+    }
 }
